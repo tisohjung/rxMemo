e5df77a520ddda2faa63b0e24544b8ea1594c374
Minho Yi <tisohjung@gmail.com>
2021-04-11 02:56:31 +0900

    implement MemoDetailViewModel

--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,31 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    let memo: Memo
     
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = DateFormatter.Style.medium
+        f.timeStyle = DateFormatter.Style.medium
+        return f
+    }()
+    
+    var contents: BehaviorSubject<[String]>
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinator, storage: MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formatter.string(from: memo.insertDate)
+        ])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
